965925839a67c54ca714f09c37b368e66a01cd12
Russ <russplaysguitar@yahoo.com>
2013-02-17 01:49:50 -0800

    small updates

--- src/js/app_auth.js
+++ src/js/app_auth.js
@@ -4,8 +4,9 @@
 var app_auth = (function () {
     'use strict';
 
-    // from: http://stackoverflow.com/questions/1403888/get-url-parameter-with-jquery
+    // returns the value of a url parameter
     var getURLParameter = function (name) {
+        // from: http://stackoverflow.com/questions/1403888/get-url-parameter-with-jquery
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[null,""])[1].replace(/\+/g, '%20'))||null;
     };
 
