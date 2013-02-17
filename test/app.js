b3c5f823b38d07a1b25a92b473f87ced86f053d5
Russ <russplaysguitar@yahoo.com>
2013-02-17 02:16:16 -0800

    clear it all out for gh-pages

--- test/app.js
+++
@@ -1,9 +0,0 @@
-/*global describe,it,expect*/
-(function () {
-    'use strict';
-    describe('A suite', function() {
-      it('contains spec with an expectation', function() {
-        expect(true).toBe(true);
-      });
-    });
-}());
