ed5b8507567a837c66f52c26a41d387d1906b6f1
Russ <russplaysguitar@yahoo.com>
2013-02-20 14:21:55 -0800

    require-ize PostCollection

--- src/js/app.js
+++ src/js/app.js
@@ -7,39 +7,10 @@ define([
     'libs/mustache',
     'sjcl',
     'app_auth',
-    'models/Post'
-], function (Backbone,_,$,Mustache,sjcl,app_auth,PostModel) {
+    'models/Post',
+    'collections/Post'
+], function (Backbone,_,$,Mustache,sjcl,app_auth,PostModel,PostCollection) {
     'use strict';
-
-    // represents a collection of posts
-    var PostCollection = Backbone.Collection.extend({
-        model: PostModel,
-        parse: function (json) {
-            // find labels that reference projects (for posts without explicit project references)
-            _.defer(_.bind(function () {
-                var projects = this.groupBy('project');
-                var projectList = _.keys(projects);
-
-                this.each(function (post) {// TODO: don't re-loop it all, just loop the new stuff?
-                    var labelsOfProjects = _.intersection(post.get('labels'), projectList);
-
-                    if (!post.get('project') && _.size(labelsOfProjects)) {
-                        post.set('project', labelsOfProjects[0]);// just take the first one referenced (for now)
-                    }
-                });
-            }, this));
-
-            // only put status posts into the collection
-            return _.filter(json, function (post) {
-                return post.type === 'https://tent.io/types/post/status/v0.1.0';
-            });
-        },
-        byTaskForProject: function (projectName) {// useful??
-            return _.groupBy(this.groupBy('project')[projectName], function (model) {
-                return model.get('task');
-            }, this);
-        }
-    });
 
     // a list of tasks for a given project
     var TasksView = Backbone.View.extend({
