c726b41b2555b98c743bf429c880b5c3333a7a61
Russ <russplaysguitar@yahoo.com>
2013-02-20 14:18:34 -0800

    require-ize Post model

--- src/js/app.js
+++ src/js/app.js
@@ -6,40 +6,10 @@ define([
     'jquery',
     'libs/mustache',
     'sjcl',
-    'app_auth'
-], function (Backbone,_,$,Mustache,sjcl,app_auth) {
+    'app_auth',
+    'models/Post'
+], function (Backbone,_,$,Mustache,sjcl,app_auth,PostModel) {
     'use strict';
-    // represents a single post
-    var PostModel = Backbone.Model.extend({
-        taskPattern: /\#([\w_\d]+)\/([\w_\d]+)/,
-        statusPattern: /\#(open|assign|close)/i,
-        labelPattern: /\#([\w_\d]+)/gi,
-        parse: function (post) {
-            var text = post.content.text,
-                taskMatches = this.taskPattern.exec(text),
-                project = taskMatches ? taskMatches[1] : null,
-                task = taskMatches ? taskMatches[2]: null;
-
-            this.set('project', project);
-            this.set('task', task);
-
-            var statusMatch = this.statusPattern.exec(text);
-            var status = statusMatch ? statusMatch[1] : null;
-            this.set('status', status);
-
-            var labels = [];
-            var match;
-            while((match = this.labelPattern.exec(text))) {
-                var label = match[1];
-                labels.push(label);
-            }
-            this.set('labels', labels);
-
-            this.set('user', post.entity);
-
-            return post;
-        }
-    });
 
     // represents a collection of posts
     var PostCollection = Backbone.Collection.extend({
