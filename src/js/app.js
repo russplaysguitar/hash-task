b776c6a35f255e3e0832c0a91fdd3602d5c649f2
Russ <russplaysguitar@yahoo.com>
2013-02-20 02:02:32 -0800

    make posting work (it needs refactoring so badly though!)
    assuming you've authenticated via app_auth.auth(entity) and called
    app_auth.finish(), this works!

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-/*global Backbone,_,$,Mustache*/
+/*global Backbone,_,$,Mustache,sjcl*/
 (function () {
     'use strict';
 
@@ -179,7 +179,7 @@
             // update DOM
             var template = 
                 '<form class="form-horizontal"><div class="control-group">' +
-                '<textarea name="comment" placeholder="Task comment" required></textarea><br />' +
+                '<textarea class="comment" name="comment" placeholder="Task comment" required></textarea><br />' +
                 '<button type="submit" class="btn btn-inverse">New Task</button>' +
                 '</div></form>'
             ;
@@ -187,7 +187,55 @@
             return this.$el;
         },
         newPost: function (evt) {
+            var AppJSON = JSON.parse(localStorage.AppJSON);
+            var matches = /^(http|https):\/\/([.\d\w\-]+)/.exec(localStorage.entity);
+            var host = matches[2];
 
+            // prepare request string for hmac signature
+            var request = {
+                method: 'POST',
+                path: '/tent/posts',
+                host: host,
+                port: 443
+            };
+            var date = new Date(),
+                timestamp = parseInt((date * 1) / 1000, 10),
+                nonce = Math.random().toString(16).substring(3),
+                request_string = [timestamp, nonce, request.method.toUpperCase(), request.path, request.host, request.port, null, null].join("\n");
+
+            // create base64-encoded hash token for authentication
+            // TODO: support other encryptions. (currently assuming sha-256-hmac)
+            var hmac = new sjcl.misc.hmac(sjcl.codec.utf8String.toBits(AppJSON.mac_key));
+            var signature = sjcl.codec.base64.fromBits(hmac.mac(request_string));
+
+            var data = {
+                "type": "https://tent.io/types/post/status/v0.1.0",
+                "published_at": timestamp,
+                "permissions": {
+                    "public": true
+                },
+                "licenses": [
+                    "http://creativecommons.org/licenses/by/3.0/"
+                ],
+                "content": {
+                    "text": this.$('.comment').val()
+                }
+            };
+
+            $.ajax({
+                url: 'https://' + request.host + request.path,
+                type: request.method,
+                contentType: 'application/vnd.tent.v0+json',
+                headers: {
+                    'Accept': 'application/vnd.tent.v0+json',
+                    'Authorization': 'MAC id="'+AppJSON.access_token+'", ts="'+timestamp+'", nonce="'+nonce+'", mac="'+signature+'"'
+                },
+                data: JSON.stringify(data),
+                success: function () {
+                    this.$('.comment').val('');
+                    Backbone.history.loadUrl(Backbone.history.fragment);// refresh page 
+                }
+            });
             return false;
         }
     });
